846cda533c899dcaa3b616fb3ca2725f63dfddd0
lalith <lalith.kumar.sbs@gmail.com>
2018-05-22 22:00:25 +0530

    Updated  sha version

--- Casks/ctools.rb
+++ Casks/ctools.rb
@@ -1,12 +1,14 @@
 cask 'ctools' do
-  version '1.0.0'
-  sha256 :no_check
+  version '0.1'
+  sha256 '1c54c638f665896cefd69bbc99c5947cf7bab5d97b748ea41ea7e22c4c6080e7'
 	depends_on cask: 'xquartz'
 	depends_on cask: 'docker'
 
-  url 'https://github.com/lksesham/homebrew-ctools/blob/master/LaunchDC.dmg'
+  url "https:////github.com/lksesham/homebrew-ctools/raw/master/LaunchDC-#{version}.dmg"
   name 'cirrustools'
   homepage 'https://github.com/lksesham/homebrew-ctools'
+	depends_on macos: '>= :yosemite'
 
 	app 'lcd.app'
+
 end
