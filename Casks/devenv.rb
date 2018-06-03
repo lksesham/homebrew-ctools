7c480dbb3a7e436f5aa360bfaecaceea7e26ee55
lalith <lalith.kumar.sbs@gmail.com>
2018-06-03 15:04:54 +0530

    new version

--- Casks/devenv.rb
+++ Casks/devenv.rb
@@ -1,6 +1,7 @@
 cask 'devenv' do
-  version '0.1'
+  version '0.2'
   sha256 'da6b4bea671b84c0e50b72228dcd44ba408468ddb0c41ed0c486402bb7da6b7f'
+  sha256 '86f38c4754a0c6cf7e57092dde3468f91406d560c566ebbb8932dca28945e3ef'
 	depends_on cask: 'xquartz'
 	depends_on cask: 'docker'
 
