50c686ea2600be0df7bd230d24cf7b1dc871b44b
lalith <lalith.kumar.sbs@gmail.com>
2018-06-03 14:24:01 +0530

    correcting version

--- Casks/ctools.rb
+++ Casks/ctools.rb
@@ -1,5 +1,5 @@
 cask 'ctools' do
-  version '0.2'
+  version '0.1'
   sha256 'da6b4bea671b84c0e50b72228dcd44ba408468ddb0c41ed0c486402bb7da6b7f'
 	depends_on cask: 'xquartz'
 	depends_on cask: 'docker'
