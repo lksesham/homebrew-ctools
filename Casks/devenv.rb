afadd9dc10040332c9805ce346dbbe402844e422
lalith <lalith.kumar.sbs@gmail.com>
2018-06-06 16:58:41 +0530

    correct shasum

--- Casks/devenv.rb
+++ Casks/devenv.rb
@@ -1,6 +1,5 @@
 cask 'devenv' do
   version '0.2'
-  sha256 'ab9fb6e06ac83f9fdfdcfe1e55b236dbe3c2a79661beaec5359b533cc583666f'
   sha256 '64ef14ba971016a1a2159119e2ba88c31ec9d9636c2758eb99b4ae9530e4575c'
 	depends_on cask: 'xquartz'
 	depends_on cask: 'docker'
